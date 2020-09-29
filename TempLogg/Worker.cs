ca6cc37df46c3266fa19305a94b7cbca0cfa95e7
Mattias Karlsson <Mattias.E.Karlsson@outlook.com>
2020-09-29 17:28:27 +0200

    Final

--- TempLogg/Worker.cs
+++ TempLogg/Worker.cs
@@ -1,19 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using MySqlX.XDevAPI.Common;
 using TempLogg.Models;
 
 namespace TempLogg
 {
-    
-
 
     public class Worker : BackgroundService
     {
@@ -44,34 +38,27 @@ namespace TempLogg
             var httpClient = HttpClientFactory.Create();
             while (!stoppingToken.IsCancellationRequested)
             {
-
                 try
                 {
-                    
                     var url = "https://api.openweathermap.org/data/2.5/onecall?lat=59.27412&units=metric&lon=15.2066&exclude=hourly,daily,minutely&appid=5bf919005c4c20e778ba98f74c7f2e33";
                     HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
 
                     if (httpResponseMessage.IsSuccessStatusCode)
                     {
-                        
                         var content = httpResponseMessage.Content;
                         var datatemp = await content.ReadAsAsync<Rootobject>();
-                        
                         
                         if (datatemp.current.temp < 20)
                         {
                             _logger.LogInformation($"The temperature is : {datatemp.current.temp}");
-                            
                         }
                         else
                         {
                             _logger.LogInformation($"Warning!! Temperature is: {datatemp.current.temp}");
-                           
                         }
-
                     }
-                    
                 }
+
                 catch(Exception ex)
                 {
                     _logger.LogInformation(ex.Message);
